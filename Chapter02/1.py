f8b67a0c99c2be2d2008be10961bfb3da4713824
김동현 <you@example.com>
2023-03-21 12:34:15 +0900

    python수업

---
+++ Chapter02/1.py
@@ -0,0 +1,21 @@
+#1.py
+#동양미래대
+#파이썬 프로그래밍
+''' 
+이것은
+여러줄로 이루어진
+주석입니다.
+'''
+print("hello world!!")
+print("it is good to see you")
+print("python" + "만세")
+print("Python!!!"*3)
+print(type(11))
+print("hello world" * 5,0)
+
+print(1,2,-5,3.14,2.71828)
+print('hi','phthon')
+
+print('23000원은','50000원 ?개','1000원 ?개')
+print('5000원',23000//5000,'개')
+print('1000원',(23000%5000)//1000,'개')
